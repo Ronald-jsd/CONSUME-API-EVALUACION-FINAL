51174e8c845bdfbc55a7e1d9d82aee685ae8375c
Ronald.jsd <ronald.santi.jd@gmail.com>
2025-02-19 23:10:09 -0500

    refactor: Delete unused comments

--- src/main/java/com/cibertec/evaluacionFinal/cibertec/controllers/StudentController.java
+++ src/main/java/com/cibertec/evaluacionFinal/cibertec/controllers/StudentController.java
@@ -29,13 +29,13 @@ public class StudentController {
                 .build();
     }
 
-
     //vista para listar estudiantes
     @GetMapping("/")
     public String index(Model model) {
         //Peticion Get a la API
-        ResponseEntity<StudentDTO[]> students = this.restTemplate
-                .getForEntity(this.apiUrl, StudentDTO[].class);
+        ResponseEntity<StudentDTO[]> students =
+                this.restTemplate
+                        .getForEntity(this.apiUrl, StudentDTO[].class);
         model.addAttribute("students", students.getBody());
         return "index";
     }
@@ -45,26 +45,18 @@ public class StudentController {
     public String findById(@PathVariable Long id, Model model) {
         // Realiza una petición GET a la API
         ResponseEntity<StudentDTO> student =
-                this.restTemplate.getForEntity(
-                        this.apiUrl + '/' + id, StudentDTO.class);
+                this.restTemplate
+                        .getForEntity(this.apiUrl + '/' + id, StudentDTO.class);
 
         // Añade el producto obtenido al modelo para que sea enviado y muestre en la vista de detalles
         model.addAttribute("student", student.getBody());
         return "details-student";
     }
 
-
-
     //Vista del formulario
     @GetMapping("/new")
     public String newStudent(Model model) {
         studentDetails(model, "Register Student" , "Register","/save",  new StudentDTO() );
-        /*
-        model.addAttribute("title", "Register Student");
-        model.addAttribute("textButton", "Register");
-        model.addAttribute("action", "/save");
-        model.addAttribute("student", new StudentDTO()); // Crea un nuevo objeto vacío de ProductDTO para ser utilizado en el formulario
-        */
         return "formStudent";
     }
 
@@ -72,14 +64,11 @@ public class StudentController {
     @PostMapping("/save")
     public String save(StudentDTO studentDTO) {
         //Consultar al backend para guardar
-        this.restTemplate.postForEntity
-                (this.apiUrl,
-                        studentDTO
-                        , StudentDTO.class);
+        this.restTemplate
+                .postForEntity(this.apiUrl, studentDTO, StudentDTO.class);
 
         return "redirect:/";
     }
-
 
     // Metodo para mostrar el formulario de edición de un producto existente
     @GetMapping("/edit/{id}")
@@ -88,15 +77,10 @@ public class StudentController {
                 .getForEntity(this.apiUrl + '/' + id, StudentDTO.class);
 
         studentDetails(model,"Edit Student", "/save",  "Save", student.getBody());
-        /*model.addAttribute("title", "Edit Student");
-        model.addAttribute("action", "/save");
-        model.addAttribute("textButton", "Save");
-        model.addAttribute("student", student.getBody());*/
 
         // Añade el producto al modelo para que se cargue en el formulario
         return "formStudent";
     }
-
 
     // Metodo para mostrar el formulario de eliminación de un producto
     @GetMapping("/viewdelete/{id}")
@@ -105,12 +89,6 @@ public class StudentController {
         ResponseEntity<StudentDTO> student = this.restTemplate
                 .getForEntity(this.apiUrl + '/' + id, StudentDTO.class);
         studentDetails(model,"Delete Student", "/delete/" + student.getBody().getId(),  "Delete", student.getBody());
-          /*
-            model.addAttribute("title", "Delete Student");
-            model.addAttribute("action", "/delete/" + student.getBody().getId());
-            model.addAttribute("textButton", "Delete");
-            model.addAttribute("student", student.getBody()); // Añade el producto al modelo para mostrar sus datos en el formulario
-            */
         return "formStudent";
     }
 
@@ -128,14 +106,8 @@ public class StudentController {
         model.addAttribute("title", "Delete Student");
         model.addAttribute("action", action);
         model.addAttribute("textButton", "Delete");
-        model.addAttribute("student",student); // Añade el Student al modelo para mostrar sus datos en el formulario
-    }
-
-    void get(Long id){
-        ResponseEntity<StudentDTO> student =
-                this.restTemplate.getForEntity(
-                        this.apiUrl + '/' + id,
-                        StudentDTO.class);
+        // Añade el Student al modelo para mostrar sus datos en el formulario
+        model.addAttribute("student",student);
     }
 
 }
